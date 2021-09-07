f76da1a56a84d08d70c612f0c9f593a37d5f4637
cabralwd <cabralwd@icloud.com>
2021-09-07 16:16:53 -0300

    import gtag

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vueMeta from "vue-meta";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
 
 Vue.use(vueMeta);
+
+Vue.use(VueGtag, {
+  config: { id: "G-M6957320JK" },
+});
 
 new Vue({
   router,
